6511568ca330a23619da43ca8bc43f98e4057fb6
Rizki Amanullah Hakim <rizkiamanullah723@gmail.com>
2024-07-02 16:20:45 +0700

    (fix) reporting remove hover

--- resources/views/pages/reporting.blade.php
+++ resources/views/pages/reporting.blade.php
@@ -158,7 +158,7 @@
                     </div>
                     <div class="card-body">
                         <div class="table-responsive w-100">
-                            <table class="table table-striped table-bordered table-hover" id="rencanaMingguan" style="width: 150%;">
+                            <table class="table table-striped table-bordered" id="rencanaMingguan" style="width: 150%;">
                                 <thead>
                                     <tr style="background-color: #fff">
                                         <th class="text-center" rowspan="2">No</th>
